31d1801ee34b583d2b1a261c11425fa30b635cf9
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2024-10-21 22:59:39 -0300

    Add bedrock response error handling

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -36,7 +36,7 @@ export function request(ctx) {
     const parsedBody = JSON.parse(ctx.result.body);
     // Extract the text content from the response
     const res = {
-      body: parsedBody.content[0].text,
+      body: parsedBody.content?.[0].text || parsedBody.message,
     };
     // Return the response
     return res;
